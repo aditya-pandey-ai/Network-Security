e7311413618bb13ce8e1a76aac4dc4ea282a5c5d
Aditya <aditya@Adityas-MacBook-Air.local>
2025-08-29 20:50:12 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import sys
@@ -19,6 +20,13 @@ if __name__ == '__main__':
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+        logging.info("Initiate the data Transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("data Transformation Completed")
+        print(data_transformation_artifact)
+
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import yaml
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -12,13 +13,38 @@ def read_yaml_file(file_path: str) -> dict:
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
 
-def write_yaml_file(file_path: str, content: object, replace: bool = False)-> None:
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
     try:
-        if replace:
-            if os.path.exists(file_path):
-                os.remove(file_path)
-            os.makedirs(os.path.dirname(file_path), exist_ok=True)
-            with open(file_path, "w") as file:
-                yaml.dump(content, file)
+        # Ensure directory exists (if any)
+        dir_name = os.path.dirname(file_path)
+        if dir_name:
+            os.makedirs(dir_name, exist_ok=True)
+
+        # If replace=False and file exists, don't overwrite
+        if not replace and os.path.exists(file_path):
+            return
+
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+def save_object(file_path: str,obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of the MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Successfully saved the save_object method of the MainUtils class")
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
